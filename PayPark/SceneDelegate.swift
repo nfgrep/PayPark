05569eb7b261e7351573e6c36933abff39312710
nfgrep <nfg2600@gmail.com>
2020-10-05 18:44:21 -0400

    Cloud Firestore added in scene delegate

--- PayPark/SceneDelegate.swift
+++ PayPark/SceneDelegate.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import SwiftUI
+import FirebaseFirestore
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     
     var userSettings = UserSettings()
+    let db = Firestore.firestore()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
